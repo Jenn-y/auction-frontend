b1d2f891842bad8c3a1e6559b95e25ac92fd8e4a
Dzenita Djulovic <djulovic.dzenita1999@gmail.com>
2021-10-28 14:04:01 +0200

    Added scroll to top when rendering new page

--- src/routes/Routes.tsx
+++ src/routes/Routes.tsx
@@ -3,14 +3,18 @@ import { Route, Switch } from "react-router-dom";
 import About from 'pages/about/About';
 import Terms_Conditions from 'pages/terms_conditions/Terms_Conditions';
 import Privacy_Policy from 'pages/privacy_policy/Privacy_Policy';
+import ScrollToTop from "utils/ScrollToTop";
 
 const Routes = () => {
 	return (
-		<Switch>
-			<Route exact path="/terms_conditions" component={Terms_Conditions} />
-			<Route exact path="/about" component={About} />
-			<Route exact path="/privacy_policy" component={Privacy_Policy} />
-		</Switch>
+		<>
+			<ScrollToTop />
+			<Switch>
+				<Route exact path="/terms_conditions" component={Terms_Conditions} />
+				<Route exact path="/about" component={About} />
+				<Route exact path="/privacy_policy" component={Privacy_Policy} />
+			</Switch>
+		</>
 	);
 };
 
