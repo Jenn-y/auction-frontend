e77d8654b2b1209112aed607c58ae23a756a8b27
Dzenita Djulovic <djulovic.dzenita1999@gmail.com>
2021-11-04 21:24:03 +0100

    Implemented login logic with validation checks

--- src/components/Login/Login.tsx
+++ src/components/Login/Login.tsx
@@ -1,8 +1,76 @@
-import LoginForm from './LoginForm';
+import { useState } from 'react'
+import { toast } from 'react-toastify';
+
+import AuthService from 'services/AuthService';
+import { validateLoginData } from 'utils/Validations';
+
+import './Login.scss';
+import '../common_style/Form.scss'
 
 const Login = () => {
+	const [user, setUser] = useState({ email: '', password: '' })
+	const [errors, setErrors] = useState({ email: '', password: '', isError: false })
+
+	const handleChange = (e: any) => {
+		setUser(Object.assign({}, user, { [e.target.name]: e.target.value }))
+	}
+
+	const handleSubmit = (e: any) => {
+		e.preventDefault();
+		if (validate()) {
+			AuthService.login(
+				user.email,
+				user.password
+			).then(
+				() => {
+					toast.success("Login sucessful!", { hideProgressBar: true });
+					window.location.replace("/")
+				}
+			)
+				.catch(() => {
+					toast.error("Wrong email or password!", { hideProgressBar: true });
+				}
+				);
+		}
+
+	};
+
+	const validate = () => {
+		setErrors({
+			email: validateLoginData(user).email,
+			password: validateLoginData(user).password,
+			isError: validateLoginData(user).isError
+		})
+		return !errors.isError
+	}
+
 	return (
-		<LoginForm />
+		<div className="form">
+			<div className="title">LOGIN</div>
+			<form onSubmit={handleSubmit}>
+				<div className="input_wrap">
+					<label>Email</label>
+					<div className="input_field">
+						<input onChange={handleChange} value={user.email} name="email" type="text" className="input" placeholder="Enter your email" />
+						<span>{errors.email}</span>
+					</div>
+				</div>
+				<div className="input_wrap">
+					<label>Password</label>
+					<div className="input_field">
+						<input onChange={handleChange} value={user.password} name="password" type="password" className="input" placeholder="Enter your password" />
+						<span>{errors.password}</span>
+					</div>
+				</div>
+				<div className="input_wrap remember-box">
+					<input type="checkbox" name="remember_me" />
+					<label htmlFor="remember_me">Remember me</label>
+				</div>
+				<div className="input_wrap">
+					<input type="submit" id="login_btn" className="btn btn-lg" value="Login" />
+				</div>
+			</form>
+		</div>
 	);
 }
 
