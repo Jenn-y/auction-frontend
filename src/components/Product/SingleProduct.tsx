3f73f961a2c2a9b2dcc7d06c9146dadfc938bee7
Dzenita Djulovic <djulovic.dzenita1999@gmail.com>
2021-11-06 19:30:19 +0100

    Added table of bidders on product overview with styling

--- src/components/Product/SingleProduct.tsx
+++ src/components/Product/SingleProduct.tsx
@@ -1,13 +1,34 @@
 import { faAngleRight, faHeart } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { useState } from 'react'
 import './SingleProduct.scss'
 
 const SingleProduct = () => {
 
+	const [detailsActive, setDetails] = useState(true)
+	const [sellerInfoActive, setSellerInfo] = useState(false)
+	const [customerRevActive, setCustomerRev] = useState(false)
+
+	const handleDetails = () => {
+		setDetails(true)
+		setSellerInfo(false)
+		setCustomerRev(false)
+	}
+
+	const handleSellerInfo = () => {
+		setDetails(false)
+		setSellerInfo(true)
+		setCustomerRev(false)
+	}
+
+	const handleCustomerRev = () => {
+		setDetails(false)
+		setSellerInfo(false)
+		setCustomerRev(true)
+	}
+
 	let images = [
-		'https://image.shutterstock.com/image-photo/happy-cheerful-young-woman-wearing-600w-613759379.jpg',
-		'https://image.shutterstock.com/image-photo/closeup-photo-amazing-short-hairdo-600w-1617540484.jpg',
-		'https://image.shutterstock.com/image-photo/image-cheerful-african-american-woman-600w-1718299927.jpg',
+		'https://media1.popsugar-assets.com/files/thumbor/CHzF5iQ31LcGCjSPu1xF0wjTypg/0x0:1500x2024/fit-in/1024x1024/filters:format_auto-!!-:strip_icc-!!-/2021/04/20/773/n/1922564/c9ce4a74607f107ac3b225.06048116_/i/Best-Women-Sneakers.jpg'
 	]
 
 	return (
@@ -51,7 +72,12 @@ const SingleProduct = () => {
 							<button>Watchlist <FontAwesomeIcon icon={faHeart} className="heart" /></button>
 						</div>
 						<div className="details-section">
-							<p>Details</p>
+							<button className={detailsActive ? 'active' : ''}
+								onClick={handleDetails} >Details</button>
+							<button className={sellerInfoActive ? 'active' : ''}
+								onClick={handleSellerInfo}>Seller Information</button>
+							<button className={customerRevActive ? 'active' : ''}
+								onClick={handleCustomerRev}>Customer Reviews</button>
 						</div>
 						<div className="item-details">
 							<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.</p>
@@ -62,6 +88,113 @@ const SingleProduct = () => {
 								<li>Sunt in culpa qui officia deserunt mollit anim id est laborum</li>
 								<li>Sed ut perspiciatis unde omnis iste natus error sit</li>
 							</ul>
+						</div>
+					</div>
+				</div>
+				<div className="row bidders">
+					<div className="col-md-12">
+						<div className="table-section">
+							<table className="table">
+								<thead>
+									<tr>
+										<th>BIDDER</th>
+										<th>DATE</th>
+										<th>BID</th>
+									</tr>
+								</thead>
+								<tbody>
+									<tr className="bidders-list">
+										<td colSpan={2} className="title">
+											<div className="thumb">
+												<img className="img-fluid" src="https://bootdey.com/img/Content/avatar/avatar7.png" alt="" />
+											</div>
+											<div className="bidder-details">
+												<div className="bidder-list-title">
+													<h5>Brooke Kelly</h5>
+												</div>
+											</div>
+										</td>
+										<td colSpan={1} className="bid-date">
+											<p className="date">4 March 2021</p>
+										</td>
+										<td colSpan={1} className="bid-amount">
+											<p>$ 120.00</p>
+										</td>
+									</tr>
+									<tr className="bidders-list">
+										<td colSpan={2} className="title">
+											<div className="thumb">
+												<img className="img-fluid" src="https://bootdey.com/img/Content/avatar/avatar7.png" alt="" />
+											</div>
+											<div className="bidder-details">
+												<div className="bidder-list-title">
+													<h5>Brooke Kelly</h5>
+												</div>
+											</div>
+										</td>
+										<td colSpan={1} className="bid-date">
+											<p className="date">4 March 2021</p>
+										</td>
+										<td colSpan={1} className="bid-amount">
+											<p>$ 120.00</p>
+										</td>
+									</tr>
+									<tr className="bidders-list">
+										<td colSpan={2} className="title">
+											<div className="thumb">
+												<img className="img-fluid" src="https://bootdey.com/img/Content/avatar/avatar7.png" alt="" />
+											</div>
+											<div className="bidder-details">
+												<div className="bidder-list-title">
+													<h5>Brooke Kelly</h5>
+												</div>
+											</div>
+										</td>
+										<td colSpan={1} className="bid-date">
+											<p className="date">4 March 2021</p>
+										</td>
+										<td colSpan={1} className="bid-amount">
+											<p>$ 120.00</p>
+										</td>
+									</tr>
+									<tr className="bidders-list">
+										<td colSpan={2} className="title">
+											<div className="thumb">
+												<img className="img-fluid" src="https://bootdey.com/img/Content/avatar/avatar7.png" alt="" />
+											</div>
+											<div className="bidder-details">
+												<div className="bidder-list-title">
+													<h5>Brooke Kelly</h5>
+												</div>
+											</div>
+										</td>
+										<td colSpan={1} className="bid-date">
+											<p className="date">4 March 2021</p>
+										</td>
+										<td colSpan={1} className="bid-amount">
+											<p>$ 120.00</p>
+										</td>
+									</tr>
+									<tr className="bidders-list">
+										<td colSpan={2} className="title">
+											<div className="thumb">
+												<img className="img-fluid" src="https://bootdey.com/img/Content/avatar/avatar7.png" alt="" />
+											</div>
+											<div className="bidder-details">
+												<div className="bidder-list-title">
+													<h5>Brooke Kelly</h5>
+												</div>
+											</div>
+										</td>
+										<td colSpan={1} className="bid-date">
+											<p className="date">4 March 2021</p>
+										</td>
+										<td colSpan={1} className="bid-amount">
+											<p>$ 120.00</p>
+										</td>
+									</tr>
+								</tbody>
+							</table>
 						</div>
 					</div>
 				</div>
