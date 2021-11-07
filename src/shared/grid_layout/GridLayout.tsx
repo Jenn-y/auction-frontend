0db9b58c27523bcf6e4382172609a56f778d5aed
Dzenita Djulovic <djulovic.dzenita1999@gmail.com>
2021-11-07 20:10:13 +0100

    Modified grid layout

--- src/shared/grid_layout/GridLayout.tsx
+++ src/shared/grid_layout/GridLayout.tsx
@@ -1,54 +1,27 @@
+import { Col, Container, Row } from 'reactstrap'
+
 import './GridLayout.scss'
 
-const GridLayout = () => {
+const GridLayout = (models: any) => {
 
 	let images = [
 		'https://media1.popsugar-assets.com/files/thumbor/CHzF5iQ31LcGCjSPu1xF0wjTypg/0x0:1500x2024/fit-in/1024x1024/filters:format_auto-!!-:strip_icc-!!-/2021/04/20/773/n/1922564/c9ce4a74607f107ac3b225.06048116_/i/Best-Women-Sneakers.jpg'
 	]
 
 	return (
-		<div className="row section">
-			<div className="col-12 col-sm-3 col-lg">
-				<img src={images[0]} alt="sneakers" />
-				<h4>Shoes Collection</h4>
-				<p>Start From <span>$59.00</span></p>
-			</div>
-			<div className="col-12 col-sm-3 col-lg">
-				<img src={images[0]} alt="sneakers" />
-				<h4>Shoes Collection</h4>
-				<p>Start From <span>$59.00</span></p>
-			</div>
-			<div className="col-12 col-sm-3 col-lg">
-				<img src={images[0]} alt="sneakers" />
-				<h4>Shoes Collection</h4>
-				<p>Start From <span>$59.00</span></p>
-			</div>
-			<div className="col-12 col-sm-3 col-lg">
-				<img src={images[0]} alt="sneakers" />
-				<h4>Shoes Collection</h4>
-				<p>Start From <span>$59.00</span></p>
-			</div>
-			<div className="col-12 col-sm-3 col-lg">
-				<img src={images[0]} alt="sneakers" />
-				<h4>Shoes Collection</h4>
-				<p>Start From <span>$59.00</span></p>
-			</div>
-			<div className="col-12 col-sm-3 col-lg">
-				<img src={images[0]} alt="sneakers" />
-				<h4>Shoes Collection</h4>
-				<p>Start From <span>$59.00</span></p>
-			</div>
-			<div className="col-12 col-sm-3 col-lg">
-				<img src={images[0]} alt="sneakers" />
-				<h4>Shoes Collection</h4>
-				<p>Start From <span>$59.00</span></p>
-			</div>
-			<div className="col-12 col-sm-3 col-lg">
-				<img src={images[0]} alt="sneakers" />
-				<h4>Shoes Collection</h4>
-				<p>Start From <span>$59.00</span></p>
-			</div>
-		</div>
+		<Container>
+			<Row className="section">
+				{models.auctions.map((model: any) => {
+					return (
+						<Col xs={3} key={model.item.itemNumber} className="card-h">
+							<img src={images[0]} alt="sneakers" />
+							<h4>{model.item.name}</h4>
+							<p>Start From <span>${model.item.startPrice}</span></p>
+						</Col>
+					)
+				})}
+			</Row>
+		</Container>
 	)
 }
 
