115a919b2946bc730a6c83aa61ef9b76fd6b8235
Dzenita Djulovic <djulovic.dzenita1999@gmail.com>
2021-11-24 13:43:50 +0100

    Added interfaces for login and registration errors

--- src/utils/Validations.tsx
+++ src/utils/Validations.tsx
@@ -1,4 +1,6 @@
 import { EMAIL_EMPTY, EMAIL_INVALID, FIRST_NAME_EMPTY, LAST_NAME_EMPTY, PASSWORD_EMPTY, PASSWORD_LENGTH } from "constants/ErrorMessages";
+import { LoginError } from "interfaces/LoginError";
+import { RegistrationError } from "interfaces/RegistrationError";
 
 export const validateEmail = (email: string) => {
 	return RegExp(/^(([^<>()\[\]\.,;:\s@\"]+(\.[^<>()\[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i).test(email)
@@ -6,7 +8,7 @@ export const validateEmail = (email: string) => {
 
 export const validateRegisterData = (registerData: any) => {
 
-	const validateErrors = { firstName: '', lastName: '', email: '', password: '', isError: false };
+	const validateErrors: RegistrationError = { firstName: '', lastName: '', email: '', password: '', isError: false };
 
 	if (!registerData.firstName || registerData.firstName.length === 0) {
 		validateErrors.firstName = FIRST_NAME_EMPTY
@@ -39,7 +41,7 @@ export const validateRegisterData = (registerData: any) => {
 
 export const validateLoginData = (loginData: any) => {
 
-	const validateErrors = { email: '', password: '', isError: false }
+	const validateErrors: LoginError = { email: '', password: '', isError: false }
 
 	if (!loginData.email || loginData.email.length === 0) {
 		validateErrors.email = EMAIL_EMPTY
