af5023ed210796c8a78fd2051d5afad1c3cc2ccb
Dzenita Djulovic <djulovic.dzenita1999@gmail.com>
2021-11-30 17:32:13 +0100

    Implemented filtering tags with removal option

--- src/components/Shop/Shop.tsx
+++ src/components/Shop/Shop.tsx
@@ -1,5 +1,6 @@
-import { faMinus, faPlus } from '@fortawesome/free-solid-svg-icons';
+import { faMinus, faPlus, faTimesCircle } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { Category } from 'interfaces/Category';
 import { useEffect, useState } from 'react';
 
 import AuctionService from 'services/AuctionService';
@@ -12,9 +13,10 @@ const Shop = (props: any) => {
 
 	const [auctions, setAuctions] = useState([])
 	const [categories, setCategories] = useState([])
-	const [subcategories, setSubcategories] = useState([])
+	const [subcategories, setSubcategories] = useState<Category[]>([])
 	const [activeCategory, setActiveCategory] = useState<string>()
-	const [showSubcategories, setShowSubcategories] = useState(false)
+	const [activeSubcategories, setActiveSubcategories] = useState<Category[]>([])
+	const [checked, setChecked] = useState(false)
 
 	useEffect(() => {
 		const categoryId = props.match.params.categoryId
@@ -64,10 +66,37 @@ const Shop = (props: any) => {
 		return activeCategory === category ? faMinus : faPlus
 	}
 
+	const isChecked = (subcategory: any) => {
+		return activeSubcategories.some((category: any) => category.id === subcategory) ? true : false
+	}
+
 	const handleCategoryClick = (categoryId: string) => {
 		getAuctionsByCategoryId(categoryId)
 		getSubcategoriesByCategoryId(categoryId)
 	}
+
+	const handleSubcategoryClick = (id: string, name: string, subcategoryOf: Category) => {
+		const subcategory = {
+            id: id,
+            name: name,
+			subcategoryOf: subcategoryOf,
+        }
+
+		if (!activeSubcategories.some((category: any) => category.id === subcategory.id)) {
+            setActiveSubcategories([...activeSubcategories, subcategory])
+        } else {
+			setActiveSubcategories(activeSubcategories.filter(category => category.id !== subcategory.id))
+		}
+	}
+
+	const onClearAllClick = () => {
+        setActiveSubcategories([]);
+		setChecked(false)
+    }
+
+	const onRemoveTagClick = (subcategoryId: string) => {
+        setActiveSubcategories(activeSubcategories.filter(category => category.id !== subcategoryId))
+    }
 	
     return (
         <div className="container">
@@ -86,8 +115,8 @@ const Shop = (props: any) => {
 										{activeCategory === category.id ?
 											subcategories.map((subcategory: any) => {
 												return (
-													<div className="subcategory">
-														<input type="checkbox" name="subcategory" />
+													<div className="subcategory" key={subcategory.id}>
+														<input type="checkbox" name="subcategory" checked={isChecked(subcategory.id)} onClick={() => handleSubcategoryClick(subcategory.id, subcategory.name, category)}/>
 														<label htmlFor="subcategory">{subcategory.name} (120)</label>
 													</div>
 												)
@@ -100,6 +129,16 @@ const Shop = (props: any) => {
 					</div>
 				</div>
 				<div className="col-12 col-sm-9 col-lg product-view">
+					<div className="tag-area">
+						{activeSubcategories.map((subcategory: any) => {
+							return (
+								<div key={subcategory.id} className="subcategory-tag">{subcategory.name} <span><FontAwesomeIcon icon={faTimesCircle} color="white" onClick={() => onRemoveTagClick(subcategory.id)} /></span></div>
+							)
+						})}
+						{activeSubcategories.length > 0 ? 
+							<div className="clear-tags" onClick={onClearAllClick}>Clear all</div> : ''
+						}
+                    </div>
 					<div>
 						<GridLayout 
 							auctions={auctions}
