f8e356a18ff61ce64db1061de5be6d9180c3e2e3
Dzenita Djulovic <djulovic.dzenita1999@gmail.com>
2021-12-03 12:04:17 +0100

    Modified API call for filtered auctions

--- src/services/AuctionService.tsx
+++ src/services/AuctionService.tsx
@@ -33,9 +33,9 @@ class AuctionService {
 			.catch(() => console.log("An error occured while fetching the auctions"));
 	}
 
-	getFilteredAuctions = (category: any, subcategories: any) => {
+	getFilteredAuctions = (categories: any, subcategories: any) => {
 		return axios 
-			.get(API_URL + `auctions/categories/filter?category=${category}&subcategories=${subcategories}`)
+			.get(API_URL + `auctions/categories/filter?categories=${categories}&subcategories=${subcategories}`)
 			.then((response: any) => {
 				return response.data;
 			})
