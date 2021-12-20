9ebd7d537e72f3f78f549d7f2bb40c590dfef1dc
Dzenita Djulovic <djulovic.dzenita1999@gmail.com>
2021-12-20 02:54:37 +0100

    Added payment and shipping details interfaces

---
+++ src/interfaces/ShippingDetails.tsx
@@ -0,0 +1,8 @@
+export interface ShippingDetails {
+	id: string;
+	streetName: string;
+	city: string;
+	state: string;
+	country: string;
+	zipCode: number;
+}
--- src/interfaces/User.tsx
+++ src/interfaces/User.tsx
@@ -1,11 +1,18 @@
+import { PaymentDetails } from "./PaymentDetails";
+import { ShippingDetails } from "./ShippingDetails";
+
 export interface User {
 	id: string;
-	username: string;
 	email: string;
 	firstName: string;
 	lastName: string;
 	password: string;
+	phoneNum: string;
+	gender: string;
+	dateOfBirth: Date;
 	createdAt: Date;
 	updatedAt: Date;
 	status: string;
+	paymentDetails: PaymentDetails;
+	shippingDetails: ShippingDetails;
 }
