de4e5d069b948cad90de6aa72f4c29a2a6fd671a
Dzenita Djulovic <djulovic.dzenita1999@gmail.com>
2021-12-30 12:58:01 +0100

    Implemented suggested fixes

--- src/services/CategoryService.tsx
+++ src/services/CategoryService.tsx
@@ -1,6 +1,7 @@
 import axios from "axios";
+import { DEV_API, PROD_API } from "./ApiConstants";
 
-const API_URL = "http://localhost:8080/api/";
+const API_URL = process.env.NODE_ENV === "development" ? DEV_API : PROD_API
 
 class CategoryService {
 
@@ -10,7 +11,7 @@ class CategoryService {
 			.then((response: any) => {
 				return response.data;
 			})
-			.catch(() => console.log("An error occured while fetching the categories"));
+			.catch(() => console.error("An error occured while fetching the categories"));
 	}
 
 	getAllCategories = () => {
@@ -19,7 +20,7 @@ class CategoryService {
 			.then((response: any) => {
 				return response.data;
 			})
-			.catch(() => console.log("An error occured while fetching the categories"));
+			.catch(() => console.error("An error occured while fetching the categories"));
 	}
 
 	getSubcategoriesByCategoryId = (categoryId: any) => {
@@ -28,7 +29,7 @@ class CategoryService {
 			.then((response: any) => {
 				return response.data;
 			})
-			.catch(() => console.log("An error occured while fetching the subcategories"));
+			.catch(() => console.error("An error occured while fetching the subcategories"));
 	}
 
 	getCategory = (id: any) => {
@@ -37,7 +38,7 @@ class CategoryService {
 			.then((response: any) => {
 				return response.data;
 			})
-			.catch(() => console.log("An error occured while fetching the category."));
+			.catch(() => console.error("An error occured while fetching the category."));
 	}
 }
 
